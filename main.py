213ae2b99e265ccee1397c3ea2f359c7818186f9
João Paulo <jpaulope@indracompany.com>
2021-09-15 10:24:20 -0300

    refactor: use env variables

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import psutil
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 battery = psutil.sensors_battery()
 percent = battery.percent
+duration = int(os.getenv('NOTIFICATION_DURATION'))
+level = int(os.getenv('BATTERY_PERCENTAGE_LEVEL_TO_NOTIFY'))
 
-if percent <= 30:
+if percent <= level:
  
     from pynotifier import Notification
 
     Notification(
-        title="Battery Low",
-        description=str(percent) + "% Battery remain!!",
-        duration=5,
+        title="Ooops",
+        description="We detected " + str(percent) + "% Battery!",
+        duration=duration,
         urgency='critical',
     ).send()
